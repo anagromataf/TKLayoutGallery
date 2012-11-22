802f1299c5b0133c47b346e1891db004cff13c4f
Tobias Kräntzer <info@tobias-kraentzer.de>
2012-11-23 00:36:14 +0100

    Initial Commit

---
+++ TKLayoutGallery/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TKLayoutGallery
+//
+//  Created by Tobias Kräntzer on 23.11.12.
+//  Copyright (c) 2012 Tobias Kräntzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TKLayoutGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TKLayoutGallery
+//
+//  Created by Tobias Kräntzer on 23.11.12.
+//  Copyright (c) 2012 Tobias Kräntzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKAppDelegate class]));
+    }
+}
